401fae65e8f5fb8c3ee7b63dcd1c321b53765e3a
Anas Rida <rida_ai@yahoo.com>
2018-06-23 18:21:40 +0300

    changed theme colors and added background pictures

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-var logo_img = document.querySelector("#logo");
+// var logo_img = document.querySelector("#logo");
 
-var images = [
-  "url('slider/anniversary-beautiful-birthday-433527.jpg')",
-  "url('slider/birthday-birthday-cake-cake-140831.jpg')",
-  "url('slider/birthday-cake-celebration-353347.jpg')",
-  "url('slider/cake-chocolate-cream-635409.jpg')"
-]
+// var images = [
+//   "url('slider/anniversary-beautiful-birthday-433527.jpg')",
+//   "url('slider/birthday-birthday-cake-cake-140831.jpg')",
+//   "url('slider/birthday-cake-celebration-353347.jpg')",
+//   "url('slider/cake-chocolate-cream-635409.jpg')"
+// ]
 
-counter = 3;
+// counter = 3;
 
-function changeImage(){
-  if (counter < images.length){
-    logo_img.style.backgroundImage = images[counter];
-    console.log(counter);
-    counter++
-  }else{
-    counter = 0
-  }
-}
+// function changeImage(){
+//   if (counter < images.length){
+//     logo_img.style.backgroundImage = images[counter];
+//     console.log(counter);
+//     counter++
+//   }else{
+//     counter = 0
+//   }
+// }
 
 
-setInterval(changeImage, 3000);
+// setInterval(changeImage, 3000);
